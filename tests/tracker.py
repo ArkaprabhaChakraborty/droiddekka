8360886816031c5e1f2b11ae49dfa946f3263fcf
ArkaprabhaChakraborty <chakrabortyarkaprabha998@gmail.com>
2021-08-26 22:48:00 +0530

    minor changes

--- tests/tracker.py
+++ tests/tracker.py
@@ -10,56 +10,60 @@ def detect(frame):
     img_edges = cv2.Canny(gray, 50, 190, 3)
     ret, img_thresh = cv2.threshold(img_edges, 254, 255, cv2.THRESH_BINARY)
     contours, heirarchies = cv2.findContours(img_thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    min_radius_thresh = 3 
-    max_radius_thresh = 30 
-
+    min_radius_thresh = 10
+    max_radius_thresh = 100
+    #blank = np.zeros(frame.shape)
+    #blank = cv2.drawContours(blank,contours,-1,(0,0,0),1)
+    #cv2.imshow("Contours",blank)
     centers = [] 
 
     for c in contours:
         (x, y), radius = cv2.minEnclosingCircle(c)
         radius = int(radius)
+        print(radius)
         if (radius > min_radius_thresh) and (radius < max_radius_thresh):
             centers.append(np.array([[x], [y]]))
-        return centers 
+        return centers,radius 
 
 def main():
     
     # Create opencv video capture object
-    VideoCap = cv2.VideoCapture('/home/arkaprabha/droiddekka/tests/video/video_randomball.avi')
+    VideoCap = cv2.VideoCapture('/home/arkaprabha/droiddekka/tests/video/random_ball_2.avi')
 
     #Variable used to control the speed of reading the video
-    ControlSpeedVar = 100  #Lowest: 1 - Highest:100
+    ControlSpeedVar = 80  #Lowest: 1 - Highest:100
 
-    HiSpeed = 100
+    HiSpeed = 80
 
     #Create KalmanFilter object KF
     k = skf(4,2) 
-    k.state_process_setter(X=np.array([1, 1, 0.1,0.1]),dt=0.15,process_noise=2.25)
+    k.state_process_setter(X=np.array([1, 1, 0.1,0.1]),dt=1,process_noise=2.25)
 
 
     while(True):
         # Read frame
         ret, frame = VideoCap.read()        
-        centers = detect(frame)
+        centers, radius = detect(frame)
         # If centroids are detected then track them
+        print(centers)
         if (len(centers) > 0):
 
             # Draw the detected circle
-            cv2.circle(frame, (int(centers[0][0]), int(centers[0][1])), 10, (0, 191, 255), 2)
+            cv2.circle(frame, (int(centers[0][0]), int(centers[0][1])), radius, (0, 191, 255), 2)
 
             # Predict
-            (X, P) = k.low_pass_predict(acc=rand.uniform(0,2),alpha = 0.5)
+            (X, P) = k.low_pass_predict()
             x,y = X[0:2]
             # Draw a rectangle as the predicted object position
-            frame = cv2.rectangle(frame, (int(x - 15), int(y - 15)), (int(x + 15), int(y + 15)), (255, 0, 0), 2)
-            cv2.putText(frame, 'Prediction', (int(x+10), int(y-35)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
+            frame = cv2.rectangle(frame, (int(x - radius-5), int(y - radius-5)), (int(x + radius + 5), int(y + radius + 5)), (255, 0, 0), 2)
+            frame = cv2.putText(frame, 'Prediction', (int(x+radius+10), int(y-35)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,0,0), 2)
             # Update
             (x1, y1) = k.update(centers[0])
             print((x1,y1))
             # Draw a rectangle as the estimated object position
-            frame = cv2.rectangle(frame, (int(x1 - 15), int(y1 - 15)), (int(x1 + 15), int(y1 + 15)), (0, 0, 255), 2)
-            cv2.putText(frame, 'Estimation', (int(x+10), int(y+10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2) 
-
+            frame = cv2.rectangle(frame, (int(x1 - radius - 5), int(y1 - radius -5)), (int(x1 + radius + 5), int(y1 + radius + 5)), (0, 0, 255), 2)
+            frame = cv2.putText(frame, 'Estimation', (int(x+radius+10), int(y+10)), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
+            #print(frame)
         cv2.imshow('image', frame)
 
         if cv2.waitKey(2) & 0xFF == ord('q'):
